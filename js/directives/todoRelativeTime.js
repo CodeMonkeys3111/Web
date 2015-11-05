90650f700c874668b09ee7536bac7b4a8152e444
icolektro5u <smartoboy31@hotmail.com>
2015-11-05 13:51:03 +0800

    Removed unused directive
    Created a directive that is not needed

--- js/directives/todoRelativeTime.js
+++
@@ -1,9 +0,0 @@
-/*global todomvc */
-'use strict';
-
-/**
-* Directive that executes an expression when the element it is applied to loses focus
-*/
-todomvc.directive('todoRelativeTime', function () {
-	
-});
